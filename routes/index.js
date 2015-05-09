85de6f404f397411489a7ec5e135db83c97923df
Eziohao <eziohao@gmail.com>
2015-05-09 18:30:36 +0800

    Finish the post
    Now users could post their blog.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
-var crypto = require('crypto'),
-  User = require('../models/user.js');
+var crypto = require('crypto');
+var User = require('../models/user.js');
+var Post=require('../models/post.js');
 var router = express.Router();
 var flash = require('connect-flash');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
+router.get('/', function(req, res) {   //mainpage
+  Post.get(null,function(err,posts){
+    if(err){
+      post=[];
+    }
+    res.render('index', {
     title: 'Main page',
     user:req.session.user,
+    posts:posts,
     success:req.flash('success').toString(),
     error:req.flash('error').toString()
+  })
+
   });
 });
-router.get('/reg', function(req, res) {
+
+
+router.get('/reg',checkNotLogin);   
+router.get('/reg', function(req, res) {  //reg page
   res.render('reg', {
     title: 'Register',
     user:req.session.user,
@@ -21,7 +32,8 @@ router.get('/reg', function(req, res) {
     error:req.flash('error').toString()
   });
 });
-router.post('/reg', function(req, res) {
+router.post('/reg',checkNotLogin);
+router.post('/reg', function(req, res) {  //reg post function
   var name = req.body.name;
   var password = req.body.password;
   var password_re = req.body.passwordRepeat;
@@ -66,7 +78,9 @@ router.post('/reg', function(req, res) {
     });
   });
 });
-router.get('/login', function(req, res) {
+
+router.get('/login',checkNotLogin);
+router.get('/login', function(req, res) { //login page
   res.render('login', {
     title: 'Login',
     user:req.session.user,
@@ -74,7 +88,9 @@ router.get('/login', function(req, res) {
     error:req.flash('error').toString()
   });
 });
-router.post('/login',function(req,res){
+
+router.post('/login',checkNotLogin);
+router.post('/login',function(req,res){  //user logins
   var md5=crypto.createHash('md5');
   var password=md5.update(req.body.password).digest('hex');
   User.get(req.body.name,function(err,user){
@@ -92,28 +108,51 @@ router.post('/login',function(req,res){
     res.redirect('/');
   })
 });
-router.get('/logout',function(req,res){
+
+router.get('/logout',checkLogin);
+router.get('/logout',function(req,res){  //logout function
   req.session.user=null;
   req.flash('success','Logout successed');
   res.redirect('/');
-})
-router.get('/post', function(req, res) {
-  res.render('index', {
+});
+
+router.get('/post',checkLogin);
+router.get('/post', function(req, res) { //post page
+  res.render('post', {
     title: 'Post',
     user: req.session.user,
+
 success: req.flash('success').toString(),
 error: req.flash('error').toString()
   });
 });
+
+router.post('/post',checkLogin);
+router.post('/post',function(req,res){
+  var currentUser=req.session.user;
+  var post=new Post(currentUser.name,req.body.title,req.body.post);
+  post.save(function(err){
+    if(err){
+      console.log('error');
+      req.flash('error',err);
+      return res.redirect('/');
+    }
+    console.log('get');
+    req.flash('success','Post successed');
+    res.redirect('/');
+  })
+})
+
 module.exports = router;
-function checkLogin(req,res,next){
+
+function checkLogin(req,res,next){  //check login status
   if(!req.session.user){
     req.flash('error','Not login');
     res.redirect('/login');
   }
   next();
 }
-function checkNotLogin(req,res,next){
+function checkNotLogin(req,res,next){ //check login status
   if(req.session.user){
     req.flash('error','Login');
     res.redirect('back');
